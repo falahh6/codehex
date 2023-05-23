08fa38d720a6a60df053d4f0a79d48c80d52f6b1
Mohammed Falah <falahsss900@gmail.com>
2023-05-23 20:20:45 +0530

    added and styled footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,17 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h4>text footer</h4>
+      <div></div>
+      <hr />
+      <div className={styles.copyrightsection}>
+        <div className={styles.firstsec}>
+          <p> &#169; Copyright 2023. All rights resevered</p>
+        </div>
+        <div className={styles.secondsec}>
+          <p>Privicy Policy</p>
+          <p>Terms & Conditions</p>
+        </div>
+      </div>
     </footer>
   );
 };
