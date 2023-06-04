6ffcff3a83e468aaf5ec8ff22dfa1163224932a6
Mohammed Falah <falahsss900@gmail.com>
2023-06-04 22:27:00 +0530

    changes added in /compiler

--- src/store/compiler-slice.js
+++ src/store/compiler-slice.js
@@ -1,7 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-const openAI_API_key = process.env.OPENAI_API_KEY;
+import { OpenAIApi, Configuration } from "openai";
+
 const compiler_API_key = process.env.REACT_APP_COMPILER_API;
+
 const compilerInitialState = {
   output: "",
   alternativeCode: "",
@@ -46,6 +48,48 @@ export const compilerOutput = createAsyncThunk(
   }
 );
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const alternativeCode = createAsyncThunk(
+  "compilerSlice/alternativeCode",
+  async () => {
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      max_tokens: 7,
+      temperature: 0,
+    });
+
+    console.log(response.data);
+    // try {
+    //   const response = await axios.post(
+    //     "https://api.openai.com/v1/engines/davinci-codex/completions",
+    //     {
+    //       prompt: "write a formal letter asking two days leave",
+    //       max_tokens: 100,
+    //       temperature: 0.7,
+    //       n: 1,
+    //       stop: "\n",
+    //     },
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Authorization:
+    //           "Bearer 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",
+    //       },
+    //     }
+    //   );
+
+    //   console.log(response.data);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
+);
+
 const compilerSlice = createSlice({
   name: "openAIAPIrequest",
   initialState: compilerInitialState,
@@ -57,6 +101,12 @@ const compilerSlice = createSlice({
     });
     builder.addCase(compilerOutput.rejected, (state, action) => {
       console.log(action.error);
+    });
+    builder.addCase(alternativeCode.fulfilled, (state, action) => {
+      console.log(action.payload);
+    });
+    builder.addCase(alternativeCode.rejected, (state, action) => {
+      console.log(action.payload);
     });
   },
 });
