8b4812553e606a4a6a238e13a680ecec24f5371f
Mohammed Falah <falahsss900@gmail.com>
2023-07-25 19:12:41 +0530

    added hamburger and drawer at landing page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,10 +2,12 @@ import Logo from "../../utils/Logo";
 import styles from "./Header.module.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Dropdown, Space } from "antd";
+import { Dropdown, Space, Drawer, Divider } from "antd";
 import { faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { userLogout } from "../../store/auth-slice";
+import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -16,6 +18,8 @@ const Header = () => {
 
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   let user = useSelector((state) => state.auth.user);
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const emailDomains = [/@gmail\.com$/, /@github\.com$/, /@anjuman\.edu\.in$/];
   for (const domain of emailDomains) {
@@ -41,8 +45,31 @@ const Header = () => {
     },
   ];
 
+  const hamburgerClickHandler = () => {
+    setDrawerOpen(true);
+    console.log("testing drawer");
+  };
+
+  const drawerCloseHandler = () => {
+    setDrawerOpen(false);
+  };
+
   return (
     <header className={styles.header}>
+      <Drawer
+        className={styles.drawer}
+        title="Login / Sign up"
+        placement={"top"}
+        closable={true}
+        onClose={drawerCloseHandler}
+        open={drawerOpen}
+        key={"placement"}
+        height={"20vh"}
+      >
+        <p>test</p>
+        {/* <Divider /> */}
+        <p>Get Started</p>
+      </Drawer>
       <Logo redirect={redirectHandler} />
 
       <ul className={styles.authItems}>
@@ -70,7 +97,9 @@ const Header = () => {
           </>
         )}
       </ul>
-      <div className={styles.hamburger}>test</div>
+      <div className={styles.hamburger} onClick={hamburgerClickHandler}>
+        <Menu />
+      </div>
     </header>
   );
 };
